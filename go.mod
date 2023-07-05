0a1d35eb727633e3108f93edb6c84a498fad7900
Tiago Posse <tiago.posse@hi.health>
2023-07-05 11:02:53 +0200

    do not archive by default. Support multiple parallel uploads

--- go.mod
+++ go.mod
@@ -3,39 +3,40 @@ module github.com/zen-io/zen-target-s3
 go 1.20
 
 require (
-	github.com/aws/aws-sdk-go-v2 v1.18.0
-	github.com/aws/aws-sdk-go-v2/config v1.18.25
-	github.com/aws/aws-sdk-go-v2/service/s3 v1.33.1
-	github.com/zen-io/zen-core v0.0.0-20230629185926-24b38b41eeb8
+	github.com/aws/aws-sdk-go-v2 v1.18.1
+	github.com/aws/aws-sdk-go-v2/config v1.18.27
+	github.com/aws/aws-sdk-go-v2/feature/s3/manager v1.11.71
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.36.0
+	github.com/zen-io/zen-core v0.0.0-20230705085957-87141151122f
 )
 
 require (
-	atomicgo.dev/cursor v0.1.1 // indirect
+	atomicgo.dev/cursor v0.1.2 // indirect
 	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.4.10 // indirect
-	github.com/aws/aws-sdk-go-v2/credentials v1.13.24 // indirect
-	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.13.3 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.33 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.27 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.34 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/v4a v1.0.25 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.13.26 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.13.4 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.34 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.28 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.35 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/v4a v1.0.26 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.9.11 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.1.28 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.27 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.14.2 // indirect
-	github.com/aws/aws-sdk-go-v2/service/sso v1.12.10 // indirect
-	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.14.10 // indirect
-	github.com/aws/aws-sdk-go-v2/service/sts v1.19.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.1.29 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.28 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.14.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.12.12 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.14.12 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.19.2 // indirect
 	github.com/aws/smithy-go v1.13.5 // indirect
 	github.com/bmatcuk/doublestar/v4 v4.6.0 // indirect
 	github.com/fatih/color v1.15.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/tiagoposse/go-sync-types v0.0.0-20230606060517-e7839c4bca50 // indirect
 	github.com/tiagoposse/go-tasklist-out v0.0.0-20230612172535-e54b6ceb9584 // indirect
-	github.com/zen-io/zen-target-archiving v0.0.0-20230630121353-2d5d712df8f7 // indirect
-	golang.org/x/crypto v0.9.0 // indirect
-	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1 // indirect
-	golang.org/x/sys v0.8.0 // indirect
-	golang.org/x/term v0.8.0 // indirect
+	golang.org/x/crypto v0.10.0 // indirect
+	golang.org/x/exp v0.0.0-20230626212559-97b1e661b5df // indirect
+	golang.org/x/sys v0.10.0 // indirect
+	golang.org/x/term v0.9.0 // indirect
 )
--- s3_file.go
+++ s3_file.go
@@ -3,16 +3,18 @@ package s3
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
+	"strings"
+	"sync"
 
 	environs "github.com/zen-io/zen-core/environments"
 	zen_targets "github.com/zen-io/zen-core/target"
-	"github.com/zen-io/zen-core/utils"
-	archives "github.com/zen-io/zen-target-archiving"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
@@ -27,73 +29,32 @@ type S3FileConfig struct {
 	Tools        map[string]string                `mapstructure:"tools" desc:"Key-Value map of tools to include when executing this target. Values can be references"`
 	Visibility   []string                         `mapstructure:"visibility" desc:"List of visibility for this target"`
 	Environments map[string]*environs.Environment `mapstructure:"environments" desc:"Deployment Environments"`
-
-	Srcs      []string `mapstructure:"srcs"`
-	Zip       *string  `mapstructure:"zip"`
-	Bucket    string   `mapstructure:"bucket"`
-	BucketKey string   `mapstructure:"bucket_key"`
+	MaxParallel  *int                             `mapstructure:"max_parallel" desc:"Maximum number of parallel uploads. Defaults to 10"`
+	Srcs         []string                         `mapstructure:"srcs"`
+	Bucket       string                           `mapstructure:"bucket"`
+	BucketPrefix string                           `mapstructure:"bucket_prefix"`
 }
 
 func (fc S3FileConfig) GetTargets(tcc *zen_targets.TargetConfigContext) ([]*zen_targets.Target, error) {
-	out := filepath.Base(fc.BucketKey)
-	srcs := map[string][]string{
-		"zip": {},
+	if fc.MaxParallel == nil {
+		fc.MaxParallel = new(int)
+		*fc.MaxParallel = 10
 	}
-
-	steps := []*zen_targets.Target{}
-	if len(fc.Srcs) != 0 {
-		acName := fmt.Sprintf("%s_archive", fc.Name)
-
-		ac, err := archives.ArchiveConfig{
-			Name:       acName,
-			Out:        "archive.zip",
-			Type:       "zip",
-			Srcs:       fc.Srcs,
-			Labels:     fc.Labels,
-			Deps:       fc.Deps,
-			Visibility: []string{fc.Name},
-			PassEnv:    fc.PassEnv,
-			SecretEnv:  fc.SecretEnv,
-			Env:        fc.Env,
-		}.GetTargets(tcc)
-		if err != nil {
-			return nil, err
-		}
-
-		steps = append(steps, ac...)
-		srcs["zip"] = []string{fmt.Sprintf(":%s", acName)}
-		fc.Deps = append(fc.Deps, fmt.Sprintf(":%s", acName))
-	} else if fc.Zip != nil {
-		srcs["zip"] = []string{*fc.Zip}
-	} else {
-		return nil, fmt.Errorf("provide either a Zip or Srcs")
-	}
-
-	steps = append(steps,
+	steps := []*zen_targets.Target{
 		zen_targets.NewTarget(
 			fc.Name,
-			zen_targets.WithOuts([]string{out}),
+			zen_targets.WithOuts([]string{"**/*"}),
 			zen_targets.WithEnvironments(fc.Environments),
-			zen_targets.WithSrcs(srcs),
+			zen_targets.WithSrcs(map[string][]string{
+				"_srcs": fc.Srcs,
+			}),
 			zen_targets.WithTargetScript("build", &zen_targets.TargetScript{
 				Deps: fc.Deps,
-				Run: func(target *zen_targets.Target, runCtx *zen_targets.RuntimeContext) error {
-					return utils.Copy(target.Srcs["zip"][0], filepath.Join(target.Cwd, target.Outs[0]))
-				},
 			}),
-
 			zen_targets.WithTargetScript("deploy", &zen_targets.TargetScript{
 				Run: func(target *zen_targets.Target, runCtx *zen_targets.RuntimeContext) error {
 					target.SetStatus("Uploading to s3 (%s)", target.Qn())
 					if !runCtx.DryRun {
-						var file *os.File
-						if f, err := os.Open(target.Outs[0]); err != nil {
-							return fmt.Errorf("opening src %s: %w", target.Outs[0], err)
-						} else {
-							file = f
-							defer file.Close()
-						}
-
 						customResolver := aws.EndpointResolverWithOptionsFunc(func(service, region string, options ...interface{}) (aws.Endpoint, error) {
 							var endpoint string
 							if val, ok := target.EnvVars()["AWS_S3_ENDPOINT"]; ok {
@@ -124,20 +85,59 @@ func (fc S3FileConfig) GetTargets(tcc *zen_targets.TargetConfigContext) ([]*zen_
 						if err != nil {
 							return fmt.Errorf("interpolating bucket name: %w", err)
 						}
-						interpolatedBucketKey, err := target.Interpolate(fc.BucketKey)
+
+						interpolatedBucketPrefix, err := target.Interpolate(fc.BucketPrefix)
 						if err != nil {
 							return fmt.Errorf("interpolating bucket key: %w", err)
 						}
-						target.Debugln("Bucket key: %s", interpolatedBucketKey)
 
-						// Upload the file to S3
-						if _, err := client.PutObject(context.TODO(), &s3.PutObjectInput{
-							Bucket: aws.String(interpolatedBucket),
-							Key:    aws.String(interpolatedBucketKey),
-							Body:   file,
-						}); err != nil {
-							return fmt.Errorf("uploading: %w", err)
+						target.Debugln("Bucket key: %s", interpolatedBucketPrefix)
+
+						// Create an uploader with the S3 client and default options
+						uploader := manager.NewUploader(client)
+
+						// Create a WaitGroup to manage concurrency
+						var wg sync.WaitGroup
+
+						// Create a buffered channel to control concurrency
+						sem := make(chan struct{}, *fc.MaxParallel)
+
+						for _, out := range target.Outs {
+							wg.Add(1)
+
+							// Acquire a token from the semaphore
+							sem <- struct{}{}
+
+							go func(f string) {
+								// Decrement the counter when the goroutine completes
+								defer wg.Done()
+
+								// Open the file for use
+								file, err := os.Open(f)
+								if err != nil {
+									log.Fatalf("failed to open file %q, %v", f, err)
+								}
+								defer file.Close()
+
+								// Use the uploader to upload the file
+								_, err = uploader.Upload(context.TODO(), &s3.PutObjectInput{
+									Bucket: aws.String(interpolatedBucket),
+									Key:    aws.String(filepath.Join(interpolatedBucketPrefix, strings.TrimPrefix(f, target.Cwd))),
+									Body:   file,
+								})
+								if err != nil {
+									log.Fatalf("failed to upload file %q, %v", f, err)
+								}
+
+								target.Debugln("successfully uploaded %q to S3\n", f)
+
+								// Release a token back to the semaphore
+								<-sem
+							}(out)
 						}
+
+						// Wait for all uploads to complete
+						wg.Wait()
 					}
 
 					return nil
@@ -182,7 +182,7 @@ func (fc S3FileConfig) GetTargets(tcc *zen_targets.TargetConfigContext) ([]*zen_
 				},
 			}),
 		),
-	)
+	}
 
 	return steps, nil
 }
