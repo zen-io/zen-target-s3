ff25ecbdfbc9c9a059dd97ee4625cd70221c4253
Tiago Posse <tiago.posse@hi.health>
2023-07-15 19:51:52 +0200

    bump zen-core, parallel upload of files

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# CHANGELOG
+
+## 0.0.4
+
+* [chore] bump zen-core
+* [feat] parallel upload of files
--- s3_file.go
+++ s3_file.go
@@ -40,11 +40,19 @@ func (fc S3FileConfig) GetTargets(tcc *zen_targets.TargetConfigContext) ([]*zen_
 		fc.MaxParallel = new(int)
 		*fc.MaxParallel = 10
 	}
+
+	fc.Labels = append(
+		fc.Labels,
+		fmt.Sprintf("zen_bucket=%s", fc.Bucket),
+		fmt.Sprintf("zen_bucket_prefix=%s", fc.BucketPrefix),
+	)
 	steps := []*zen_targets.Target{
 		zen_targets.NewTarget(
 			fc.Name,
 			zen_targets.WithOuts([]string{"**/*"}),
 			zen_targets.WithEnvironments(fc.Environments),
+			zen_targets.WithLabels(fc.Labels),
+			zen_targets.WithVisibility(fc.Visibility),
 			zen_targets.WithSrcs(map[string][]string{
 				"_srcs": fc.Srcs,
 			}),
@@ -54,91 +62,59 @@ func (fc S3FileConfig) GetTargets(tcc *zen_targets.TargetConfigContext) ([]*zen_
 			zen_targets.WithTargetScript("deploy", &zen_targets.TargetScript{
 				Run: func(target *zen_targets.Target, runCtx *zen_targets.RuntimeContext) error {
 					target.SetStatus("Uploading to s3 (%s)", target.Qn())
-					if !runCtx.DryRun {
-						customResolver := aws.EndpointResolverWithOptionsFunc(func(service, region string, options ...interface{}) (aws.Endpoint, error) {
-							var endpoint string
-							if val, ok := target.EnvVars()["AWS_S3_ENDPOINT"]; ok {
-								endpoint = val
-							} else {
-								endpoint = "https://s3.eu-central-1.amazonaws.com"
+
+					client, bucket, prefix, err := loadAwsConfig(target)
+					if err != nil {
+						return err
+					}
+
+					// Create an uploader with the S3 client and default options
+					uploader := manager.NewUploader(client)
+
+					// Create a WaitGroup to manage concurrency
+					var wg sync.WaitGroup
+
+					// Create a buffered channel to control concurrency
+					sem := make(chan struct{}, *fc.MaxParallel)
+
+					for _, out := range target.Outs {
+						wg.Add(1)
+
+						// Acquire a token from the semaphore
+						sem <- struct{}{}
+
+						go func(f string) error {
+							// Decrement the counter when the goroutine completes
+							defer wg.Done()
+
+							// Open the file for use
+							file, err := os.Open(f)
+							if err != nil {
+								return fmt.Errorf("failed to open file %q, %v", f, err)
 							}
-							if service == s3.ServiceID && region == "eu-central-1" {
-								return aws.Endpoint{
-									PartitionID:   "aws",
-									URL:           endpoint,
-									SigningRegion: "eu-central-1",
-								}, nil
-							}
-							// returning EndpointNotFoundError will allow the service to fallback to it's default resolution
-							return aws.Endpoint{}, &aws.EndpointNotFoundError{}
-						})
-						cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithEndpointResolverWithOptions(customResolver))
-						if err != nil {
-							return fmt.Errorf("loading aws config: %w", err)
-						}
+							defer file.Close()
 
-						client := s3.NewFromConfig(cfg, func(o *s3.Options) {
-							o.UsePathStyle = true
-						})
-						interpolatedBucket, err := target.Interpolate(fc.Bucket)
-						target.Debugln("Bucket: %s", interpolatedBucket)
-						if err != nil {
-							return fmt.Errorf("interpolating bucket name: %w", err)
-						}
-
-						interpolatedBucketPrefix, err := target.Interpolate(fc.BucketPrefix)
-						if err != nil {
-							return fmt.Errorf("interpolating bucket key: %w", err)
-						}
-
-						target.Debugln("Bucket key: %s", interpolatedBucketPrefix)
-
-						// Create an uploader with the S3 client and default options
-						uploader := manager.NewUploader(client)
-
-						// Create a WaitGroup to manage concurrency
-						var wg sync.WaitGroup
-
-						// Create a buffered channel to control concurrency
-						sem := make(chan struct{}, *fc.MaxParallel)
-
-						for _, out := range target.Outs {
-							wg.Add(1)
-
-							// Acquire a token from the semaphore
-							sem <- struct{}{}
-
-							go func(f string) {
-								// Decrement the counter when the goroutine completes
-								defer wg.Done()
-
-								// Open the file for use
-								file, err := os.Open(f)
-								if err != nil {
-									log.Fatalf("failed to open file %q, %v", f, err)
-								}
-								defer file.Close()
-
+							if !runCtx.DryRun {
 								// Use the uploader to upload the file
 								_, err = uploader.Upload(context.TODO(), &s3.PutObjectInput{
-									Bucket: aws.String(interpolatedBucket),
-									Key:    aws.String(filepath.Join(interpolatedBucketPrefix, strings.TrimPrefix(f, target.Cwd))),
+									Bucket: aws.String(bucket),
+									Key:    aws.String(filepath.Join(prefix, strings.TrimPrefix(f, target.Cwd))),
 									Body:   file,
 								})
 								if err != nil {
-									log.Fatalf("failed to upload file %q, %v", f, err)
+									return fmt.Errorf("failed to upload file %q, %v", f, err)
 								}
 
 								target.Debugln("successfully uploaded %q to S3\n", f)
-
-								// Release a token back to the semaphore
-								<-sem
-							}(out)
-						}
-
-						// Wait for all uploads to complete
-						wg.Wait()
+							}
+							// Release a token back to the semaphore
+							<-sem
+							return nil
+						}(out)
 					}
+
+					// Wait for all uploads to complete
+					wg.Wait()
 
 					return nil
 				},
@@ -146,38 +122,53 @@ func (fc S3FileConfig) GetTargets(tcc *zen_targets.TargetConfigContext) ([]*zen_
 
 			zen_targets.WithTargetScript("remove", &zen_targets.TargetScript{
 				Run: func(target *zen_targets.Target, runCtx *zen_targets.RuntimeContext) error {
-					// TODO
+					client, bucket, prefix, err := loadAwsConfig(target)
+					if err != nil {
+						return err
+					}
+					// Create a WaitGroup to manage concurrency
+					var wg sync.WaitGroup
 
-					// if runCtx.Env == "" {
-					// 	if len(tc.Environments) > 0 {
-					// 		return fmt.Errorf("you must specify an environment to deploy")
-					// 	}
+					// Create a buffered channel to control concurrency
+					sem := make(chan struct{}, *fc.MaxParallel)
 
-					// 	runCtx.Env = "_default"
-					// }
+					for _, out := range target.Outs {
+						wg.Add(1)
 
-					// execEnv := runCtx.GetEnvironmentVariables(ctx, tc.Environments[runCtx.Env])
+						// Acquire a token from the semaphore
+						sem <- struct{}{}
 
-					// if err := initFn(ctx, runCtx.Env, execEnv); err != nil {
-					// 	return fmt.Errorf("executing init: %s", err)
-					// }
+						go func(f string) {
+							// Decrement the counter when the goroutine completes
+							defer wg.Done()
 
-					// if runCtx.DryRun {
-					// 	if err := planFn(ctx, runCtx.Env, execEnv, true); err != nil {
-					// 		return fmt.Errorf("executing plan: %s", err)
-					// 	}
-					// } else {
+							// Open the file for use
+							file, err := os.Open(f)
+							if err != nil {
+								log.Fatalf("failed to open file %q, %v", f, err)
+							}
+							defer file.Close()
 
-					// 	applyCmd := exec.Command(ctx.Tools["terraform"], "destroy", "-auto-approve")
-					// 	applyCmd.Dir = fmt.Sprintf("%s/%s", ctx.Cwd, runCtx.Env)
-					// 	applyCmd.Env = execEnv
-					// 	applyCmd.Stdout = ctx.Out.GetLogger()
-					// 	applyCmd.Stderr = ctx.Out.GetLogger()
-					// 	if err := applyCmd.Run(); err != nil {
-					// 		return fmt.Errorf("running apply: %s", err.Error())
-					// 	}
-					// }
+							if !runCtx.DryRun {
+								input := &s3.DeleteObjectInput{
+									Bucket: aws.String(bucket),
+									Key:    aws.String(filepath.Join(prefix, strings.TrimPrefix(f, target.Cwd))),
+								}
 
+								_, err = client.DeleteObject(context.TODO(), input)
+								if err != nil {
+									log.Fatalf("failed to delete object, %v", err)
+								}
+
+								target.Debugln("successfully deleted %s to S3", f)
+							}
+							// Release a token back to the semaphore
+							<-sem
+						}(out)
+					}
+
+					// Wait for all uploads to complete
+					wg.Wait()
 					return nil
 				},
 			}),
@@ -185,4 +176,55 @@ func (fc S3FileConfig) GetTargets(tcc *zen_targets.TargetConfigContext) ([]*zen_
 	}
 
 	return steps, nil
+}
+
+func loadAwsConfig(target *zen_targets.Target) (*s3.Client, string, string, error) {
+	customResolver := aws.EndpointResolverWithOptionsFunc(func(service, region string, options ...interface{}) (aws.Endpoint, error) {
+		var endpoint string
+		if val, ok := target.EnvVars()["AWS_S3_ENDPOINT"]; ok {
+			endpoint = val
+		} else {
+			endpoint = "https://s3.eu-central-1.amazonaws.com"
+		}
+
+		if service == s3.ServiceID && region == "eu-central-1" {
+			return aws.Endpoint{
+				PartitionID:   "aws",
+				URL:           endpoint,
+				SigningRegion: "eu-central-1",
+			}, nil
+		}
+		// returning EndpointNotFoundError will allow the service to fallback to it's default resolution
+		return aws.Endpoint{}, &aws.EndpointNotFoundError{}
+	})
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithEndpointResolverWithOptions(customResolver))
+	if err != nil {
+		return nil, "", "", fmt.Errorf("loading aws config: %w", err)
+	}
+
+	client := s3.NewFromConfig(cfg, func(o *s3.Options) {
+		o.UsePathStyle = true
+	})
+
+	var bucket, prefix string
+	for _, label := range target.Labels {
+		if strings.HasPrefix(label, "zen_bucket=") {
+			interpolated, err := target.Interpolate(strings.TrimPrefix(label, "zen_bucket="))
+			if err != nil {
+				return nil, "", "", fmt.Errorf("interpolating bucket name: %w", err)
+			}
+			bucket = interpolated
+		} else if strings.HasPrefix(label, "zen_prefix=") {
+			interpolated, err := target.Interpolate(strings.TrimPrefix(label, "zen_prefix="))
+			if err != nil {
+				return nil, "", "", fmt.Errorf("interpolating bucket key prefix: %w", err)
+			}
+
+			prefix = interpolated
+		}
+	}
+	target.Debugln("Bucket: %s", bucket)
+	target.Debugln("Bucket key: %s", prefix)
+
+	return client, bucket, prefix, nil
 }
